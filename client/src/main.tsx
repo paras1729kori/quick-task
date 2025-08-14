02cb96742190df7d6f6180fba53155ff672a6811
Paras Kori <paras@healiom.com>
2025-08-14 12:24:14 +0530

    fix: added default import for AuthProvider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { AuthProvider } from "./contexts";
+import AuthProvider from "./contexts/AuthProvider";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
